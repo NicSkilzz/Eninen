6aee8516a0c9d2072ed344b7c5b277bb0989a001
Nico Hellthaler <nico.hellthaler@web.de>
2024-06-10 10:06:57 +0200

    Add chess demo

--- src/foo.cpp
+++ src/foo.cpp
@@ -1,67 +1,53 @@
-// wxWidgets "Hello world" Program
-// For compilers that support precompilation, includes "wx/wx.h".
-#include <wx/wxprec.h>
-#ifndef WX_PRECOMP
-    #include <wx/wx.h>
-#endif
-class MyApp: public wxApp
-{
+#include <wx/wx.h>
+
+class ChessBoardFrame : public wxFrame {
+public:
+    ChessBoardFrame(const wxString& title);
+
+private:
+    void OnPaint(wxPaintEvent& event);
+    wxDECLARE_EVENT_TABLE();
+};
+
+wxBEGIN_EVENT_TABLE(ChessBoardFrame, wxFrame)
+EVT_PAINT(ChessBoardFrame::OnPaint)
+wxEND_EVENT_TABLE()
+
+class wxWidgetsApp: public wxApp {
 public:
     virtual bool OnInit();
 };
-class MyFrame: public wxFrame
-{
-public:
-    MyFrame(const wxString& title, const wxPoint& pos, const wxSize& size);
-private:
-    void OnHello(wxCommandEvent& event);
-    void OnExit(wxCommandEvent& event);
-    void OnAbout(wxCommandEvent& event);
-    wxDECLARE_EVENT_TABLE();
-};
-enum
-{
-    ID_Hello = 1
-};
-wxBEGIN_EVENT_TABLE(MyFrame, wxFrame)
-    EVT_MENU(ID_Hello,   MyFrame::OnHello)
-    EVT_MENU(wxID_EXIT,  MyFrame::OnExit)
-    EVT_MENU(wxID_ABOUT, MyFrame::OnAbout)
-wxEND_EVENT_TABLE()
-wxIMPLEMENT_APP(MyApp);
-bool MyApp::OnInit()
-{
-    MyFrame *frame = new MyFrame( "Hello World", wxPoint(50, 50), wxSize(450, 340) );
-    frame->Show( true );
+
+wxIMPLEMENT_APP(wxWidgetsApp);
+
+bool wxWidgetsApp::OnInit() {
+    ChessBoardFrame* frame = new ChessBoardFrame("Chess Board");
+    frame->Show(true);
     return true;
 }
-MyFrame::MyFrame(const wxString& title, const wxPoint& pos, const wxSize& size)
-        : wxFrame(NULL, wxID_ANY, title, pos, size)
-{
-    wxMenu *menuFile = new wxMenu;
-    menuFile->Append(ID_Hello, "&Hello...\tCtrl-H",
-                     "Help string shown in status bar for this menu item");
-    menuFile->AppendSeparator();
-    menuFile->Append(wxID_EXIT);
-    wxMenu *menuHelp = new wxMenu;
-    menuHelp->Append(wxID_ABOUT);
-    wxMenuBar *menuBar = new wxMenuBar;
-    menuBar->Append( menuFile, "&File" );
-    menuBar->Append( menuHelp, "&Help" );
-    SetMenuBar( menuBar );
-    CreateStatusBar();
-    SetStatusText( "Welcome to wxWidgets!" );
-}
-void MyFrame::OnExit(wxCommandEvent& event)
-{
-    Close( true );
-}
-void MyFrame::OnAbout(wxCommandEvent& event)
-{
-    wxMessageBox( "This is a wxWidgets' Hello world sample",
-                  "About Hello World", wxOK | wxICON_INFORMATION );
-}
-void MyFrame::OnHello(wxCommandEvent& event)
-{
-    wxLogMessage("Hello world from wxWidgets!");
+
+ChessBoardFrame::ChessBoardFrame(const wxString& title)
+    : wxFrame(NULL, wxID_ANY, title, wxDefaultPosition, wxSize(800, 800)) {}
+
+void ChessBoardFrame::OnPaint(wxPaintEvent& event) {
+    wxPaintDC dc(this);
+    wxBrush whiteBrush(*wxWHITE_BRUSH);
+    wxBrush blackBrush(*wxBLACK_BRUSH);
+
+    int squareSize = 100;
+    bool isWhite = true;
+
+    for (int row = 0; row < 8; ++row) {
+        for (int col = 0; col < 8; ++col) {
+            if (isWhite) {
+                dc.SetBrush(whiteBrush);
+            } else {
+                dc.SetBrush(blackBrush);
+            }
+
+            dc.DrawRectangle(col * squareSize, row * squareSize, squareSize, squareSize);
+            isWhite = !isWhite;
+        }
+        isWhite = !isWhite;
+    }
 }
